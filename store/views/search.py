be6869d5931b17fc5fde777e6ecae478b9af621a
CosmoKnight <d.dragoon003@gmail.com>
2023-01-06 21:24:42 +0530

    edited frontend

--- store/views/search.py
+++ store/views/search.py
@@ -7,40 +7,31 @@ from store.models.corousel import Corousel
 from store.models.banarasphoto import BanarasPhoto
 from store.models.customer import Customer
 from store.models.wishlist import Wishlist
+from django.db.models import Q
 
 data={}
 data['address']='Ck 13/14 Satti Chautra Chowk Varanasi'
 data['phone_no']='918957451402'
 data['email']='sethsarees@gmail.com'
 
-def search(request):
-    customer_id=request.session.get('customer')
-    wishlist_len=0
-    if customer_id:
-        customer=Customer.objects.get(id=customer_id)
-        wishlist_len=len(Wishlist.objects.filter(customer=customer_id))
-    else:
-        print("nothing")
-    data['wishlist_len']=wishlist_len
 
-    query=None
-    query=request.GET['search']
-    categories=Category.get_all_categories()
-    p1=Product.objects.filter(description__icontains=query)
-    p2=Product.objects.filter(name__icontains=query)
-    c1=Category.objects.filter(description__icontains=query)
-    c2=Category.objects.filter(name__icontains=query)
-    products=p1
-    for c in c1:
-        ps=Product.objects.filter(category=c.id)
-        products=products.union(ps)    
-    for c in c2:
-        ps=Product.get_all_products_by_categoryid(c.id)
-        products=products.union(ps)    
-    products=products.union(p2)
-    data['query']="Your search results for : " +query
-    data['categories']=categories
-    data['products']=products    
-    return render(request,'search.html',data)
-    
+def search(request):
+    customer_id = request.session.get('customer')
+    wishlist_len = 0
+    if customer_id:
+        customer = Customer.objects.get(id=customer_id)
+        wishlist_len = len(Wishlist.objects.filter(customer=customer_id))
+    data['wishlist_len'] = wishlist_len
+    query = request.GET['search']
+    categories = Category.get_all_categories()
+    products = Product.objects.filter(
+        Q(description__icontains=query) |
+        Q(name__icontains=query) |
+        Q(category__description__icontains=query) |
+        Q(category__name__icontains=query)
+    )
+    data['query'] = "Your search results for: " + query
+    data['categories'] = categories
+    data['products'] = products
+    return render(request, 'search.html', data)
     
