36d3c7086692c671bb263ba375a36f9dc7c4fc87
cosmoKnight333 <cosmoknight333@gmail.com>
2023-01-18 10:24:57 +0530

    ready for hosting on heroku2

--- sethsarees/settings.py
+++ sethsarees/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'image/uploaded/'
 MEDIA_ROOT = BASE_DIR
 
