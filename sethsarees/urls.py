23f0321fb0549486dacedb8d41e4ec6820df2a35
cosmoKnight333 <cosmoknight333@gmail.com>
2023-01-18 11:38:19 +0530

    ready for hosting on heroku8

--- sethsarees/urls.py
+++ sethsarees/urls.py
@@ -18,14 +18,9 @@ from django.urls import path,include
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += staticfiles_urlpatterns()
